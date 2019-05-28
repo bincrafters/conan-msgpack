5f13016bd555c9a7f87b85d92a188f35c9d257c3
Uilian Ries <uilianries@gmail.com>
2019-05-28 10:10:05 -0300

    Add option for header only
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- build.py
+++ build.py
@@ -2,11 +2,15 @@
 # -*- coding: utf-8 -*-
 
 
-from bincrafters import build_template_header_only
+from bincrafters import build_template_default
+from conans import tools
+import os
 
 if __name__ == "__main__":
 
-    builder = build_template_header_only.get_builder()
+    builder = build_template_default.get_builder(pure_c=True)
+
+    if tools.os_info.is_linux and os.getenv("HEADER_ONLY", False) == "1":
+        builder.add({}, {"msgpack:header_only" : True}, {}, {})
 
     builder.run()
-    
--- conanfile.py
+++ conanfile.py
@@ -1,34 +1,82 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from conans import ConanFile, tools
+from conans import ConanFile, CMake, tools
 import os
 
 
 class MsgpackConan(ConanFile):
     name = "msgpack"
-    version = "3.1.1"
+    version = "3.2.0"
     description = "The official C++ library for MessagePack"
     url = "https://github.com/bincrafters/conan-msgpack"
+    homepage = "https://github.com/msgpack/msgpack-c"
+    author = "Bincrafters <bincrafters@gmail.com>"
+    topics = ("conan", "msgpack", "message-pack", "serialization")
     license = "BSL-1.0"
     exports = ["LICENSE.md"]
-    source_subfolder = "source_subfolder"
+    exports_sources = "CMakeLists.txt"
+    generators = "cmake"
+    settings = "os", "arch", "build_type", "compiler"
+    options = {"fPIC": [True, False], "shared": [True, False], "header_only": [True, False], "with_boost": [True, False]}
+    default_options = {"fPIC": True, "shared": False, "header_only": False, "with_boost": False}
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    @property
+    def _build_subfolder(self):
+        return "build_subfolder"
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def configure(self):
+        del self.settings.compiler.libcxx
+        if self.options.header_only:
+            self.settings.clear()
+            del self.options.shared
+            del self.options.fPIC
+            del self.options.with_boost
+
+    def requirements(self):
+        if not self.options.header_only and self.options.with_boost:
+            self.requires.add("boost/1.69.0@conan/stable")
 
     def source(self):
-        source_url = "https://github.com/msgpack/msgpack-c"
+        sha256 = "fbaa28c363a316fd7523f31d1745cf03eab0d1e1ea5a1c60aa0dffd4ce551afe"
         archive_name = self.name + "-" + self.version
         tools.get("{0}/releases/download/cpp-{1}/{2}.tar.gz"
-            .format(source_url,  self.version, archive_name))
+            .format(self.homepage,  self.version, archive_name), sha256=sha256)
+        os.rename(archive_name, self._source_subfolder)
 
-        os.rename(archive_name, self.source_subfolder)
+    def _configure_cmake(self):
+        cmake = CMake(self)
+        cmake.definitions["MSGPACK_BOOST"] = self.options.with_boost
+        cmake.definitions["MSGPACK_32BIT"] = self.settings.arch == "x86"
+        cmake.definitions["MSGPACK_BUILD_EXAMPLE"] = False
+        cmake.definitions["MSGPACK_BUILD_TESTS"] = False
+        cmake.configure(build_folder=self._build_subfolder)
+        return cmake
 
     def build(self):
-        pass  # silence warning
+        if not self.options.header_only:
+            cmake = self._configure_cmake()
+            cmake.build()
 
     def package(self):
-        include_dir = os.path.join(self.source_subfolder, "include")
-        self.copy("*.h", dst="include", src=include_dir)
-        self.copy("*.hpp", dst="include", src=include_dir)
+        self.copy("LICENSE_1_0.txt", dst="licenses", src=self._source_subfolder)
+        if self.options.header_only:
+            self.copy("*.h", dst=".", src=self._source_subfolder)
+            self.copy("*.hpp", dst=".", src=self._source_subfolder)
+        else:
+            cmake = self._configure_cmake()
+            cmake.install()
+            tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
 
-    def package_id(self):
-        self.info.header_only()
+    def package_info(self):
+        if self.options.header_only:
+            self.info.header_only()
+        else:
+            self.cpp_info.libs = tools.collect_libs(self)
